ce9226b447a3870547486588b632b96e26f79cc8
Error-God <kumargovinda08@gmail.com>
2024-07-22 21:00:12 +0530

    Initial Commit

---
+++ SplitNTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SplitNTrack
+//
+//  Created by Kumar, Govinda on 22/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SplitNTrack/SplitNTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitNTrackApp.swift
+//  SplitNTrack
+//
+//  Created by Kumar, Govinda on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitNTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
